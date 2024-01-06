d3bb8b5e6e6c01461f4b0455f7daa485fd38c8c6
Vatoe <vatoencube@gmail.com>
2024-01-06 14:56:53 +0000

    successfully migrated tables in DB

--- database/migrations/2024_01_06_084427_create_post_attachments_table.php
+++ database/migrations/2024_01_06_084427_create_post_attachments_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('post_attachments', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('post_id')->constrained('posts');
+            $table->string('name',255);
+            $table->string('path',255);
+            $table->string('mime',25);
+           $table->foreignId('created_by')->constrained('users');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
