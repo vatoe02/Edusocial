d867ec7ebf57869914016cee6565e78ae05d4a52
Vatoe <vatoencube@gmail.com>
2024-01-09 13:22:58 +0000

    Clean unnecessary routes and Home page

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+
+});
+
+</script>
+
+<template>
+    <Head title="Edu Social" />
+    Home page
+
+</template>
+
+<style>
+
+</style>
